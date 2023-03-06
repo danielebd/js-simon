6200be8294ffa8a9dd55d9db6bd213cfacade771
danielebd <danielebalebalestro@gmail.com>
2023-03-06 15:32:56 +0100

    generazione array numeri randomici

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createRandomNumber(ranNumb) {
+
+    const memoryNumber = document.getElementById('memory-number');
+    let arrayMemoryNumber = [];
+    for (let i = 0; arrayMemoryNumber.length < ranNumb; i++) {
+        const randomNumber = Math.floor(Math.random() * 20) + 1;
+        if (!arrayMemoryNumber.includes(randomNumber)) {
+            arrayMemoryNumber.push(randomNumber);
+            memoryNumber.innerHTML += ` ${randomNumber}`;
+        }
+        else {
+        }
+    }
+    return arrayMemoryNumber;
+}
+createRandomNumber(5)
+
+let seconds = 30;
+const clock = setInterval(function() {
+    console.log(seconds);
+    if(seconds === 0){
+        clearInterval(clock)
+        document.getElementById('memory-number').classList.add('none')
+    }
+    else {
+        seconds--;
+    }
+}, 1000);
+
+
